9c3158f1407eacf44706ae0cd7e106bee403d766
Oliver Freudrich <info@oliverfreudrich.com>
2020-09-04 15:48:58 +0200

    + added comments

--- middlewares/auth.ts
+++ middlewares/auth.ts
@@ -5,8 +5,12 @@ export default class Auth extends WebActionMiddleware {
         super();
     }
 
+    // this will run before each action where you added this as middleware
     async beforeRun({ action }: { action: WebAction }): Promise<void> {
+        // log out a message + the action which was called
         console.log('Hey I will run before your action!', action);
+
+        // throw an error on purpose to show a 500 for the action
         throw new Error('Hey I will crash!');
     }
 }
