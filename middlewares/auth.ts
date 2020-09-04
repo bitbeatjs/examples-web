22cbd02178e62752972b1693df14be1433cb2e96
Oliver Freudrich <info@oliverfreudrich.com>
2020-09-04 15:53:00 +0200

    + added example for printing routes

--- middlewares/auth.ts
+++ middlewares/auth.ts
@@ -1,4 +1,5 @@
-import { WebAction, WebActionMiddleware } from '@bitbeat/web';
+import { getInstance } from '@bitbeat/core';
+import { WebAction, WebActionMiddleware, WebServer } from '@bitbeat/web';
 
 export default class Auth extends WebActionMiddleware {
     constructor() {
@@ -9,6 +10,9 @@ export default class Auth extends WebActionMiddleware {
     async beforeRun({ action }: { action: WebAction }): Promise<void> {
         // log out a message + the action which was called
         console.log('Hey I will run before your action!', action);
+
+        // show the available routes as tree to visualize whats possible
+        console.log(getInstance(WebServer)?.runtime?.printRoutes());
 
         // throw an error on purpose to show a 500 for the action
         throw new Error('Hey I will crash!');
